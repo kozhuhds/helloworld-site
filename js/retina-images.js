67ab216cf63056c9a8a85dd2b1f7fba727780bc8
Dmitry Kozhukh <kozhuhds@Dmitrys-MacBook.local>
2014-01-31 15:56:47 +0300

    fix retina width, add map

--- js/retina-images.js
+++ js/retina-images.js
@@ -3,11 +3,12 @@ var RetinaImages = (function (){
 	var pixelRatio = !!window.devicePixelRatio ? window.devicePixelRatio : 1;
 
 	var setSrc = function (attr){
-		if (pixelRatio > 1){
+		if (pixelRatio == 1){
 			for(var i=0; i < images.length; i++){
 				if(images[i].getAttribute(attr)){
 					images[i].src = images[i].getAttribute(attr)
 						.replace('.', '@2x.');
+					images[i].setAttribute('class', 'retina-image');
 				}
 			}
 		}else{
