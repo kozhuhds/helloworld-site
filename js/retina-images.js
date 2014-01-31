d377e090a3f2ff060b70f40601675b0b9ff8ea24
Dmitry Kozhukh <kozhuhds@Dmitrys-MacBook.local>
2014-01-31 13:07:33 +0300

    contacts page and loading retina images .js

---
+++ js/retina-images.js
@@ -0,0 +1,33 @@
+var RetinaImages = (function (){
+	var images = [];
+	var pixelRatio = !!window.devicePixelRatio ? window.devicePixelRatio : 1;
+
+	var setSrc = function (attr){
+		if (pixelRatio > 1){
+			for(var i=0; i < images.length; i++){
+				if(images[i].getAttribute(attr)){
+					images[i].src = images[i].getAttribute(attr)
+						.replace('@2x.', '.')
+						.replace('.', '@2x.');
+				}
+			}
+		}else{
+			for(var i=0; i < images.length; i++){
+				images[i].src = images[i].getAttribute(attr);
+			}
+		}
+	};
+
+	var Init = {
+		loadImages: function (){
+			images = document.getElementsByTagName('img');
+		}
+	};
+
+	return {
+		init: function (attr){
+			Init.loadImages();
+			setSrc(attr);
+		}
+	}
+})();
